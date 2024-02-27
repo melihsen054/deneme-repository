ca037384b5b70b23b42f469345c6c856a30451e8
Melih Şen <ee.melihsen@gmail.com>
2024-02-27 23:21:01 +0300

    core module added

--- demo1/demo-core/src/main/java/org/melihsen/DemoApplication.java
+++ demo1/demo-core/src/main/java/org/melihsen/DemoApplication.java
@@ -1,4 +1,4 @@
-package org.melihsen.demo;
+package org.melihsen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- demo1/demo-core/src/main/java/org/melihsen/controllers/DummyRestController.java
+++ demo1/demo-core/src/main/java/org/melihsen/controllers/DummyRestController.java
@@ -1,4 +1,4 @@
-package org.melihsen.demo.controllers;
+package org.melihsen.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
